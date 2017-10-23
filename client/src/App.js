8951e0b856dae7e8aab251e2e9530bd403524f91
John Desrosiers <johndrunner@yahoo.com>
2017-10-23 15:51:24 -0700

    setup mui. remove unused files. moved PlacesSearch component file into components dir

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import PlacesSearchComponent from './placesSearch/PlacesSearchContainer';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import PlacesSearchComponent from './components/placesSearch/PlacesSearchContainer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <PlacesSearchComponent />
-      </div>
-    );
-  }
+    render () {
+        return (
+            <MuiThemeProvider>
+                <PlacesSearchComponent />
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
