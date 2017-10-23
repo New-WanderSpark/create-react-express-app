1f6f2a615b111fb33599392b4c86db31f605d5b0
Julie Ching <juliemching@gmail.com>
2017-10-22 22:29:51 -0700

    update the import for login in src/index

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Login from './login/componet/Login'
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
