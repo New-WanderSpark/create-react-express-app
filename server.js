52ad5d368597d940d9dd4750e8e35a41ce7f4f87
Julie Ching <juliemching@gmail.com>
2017-10-23 19:43:59 -0700

    routing of signin and login
    added core passport files, waiting on models and react router.

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ const bodyParser = require( 'body-parser' );
 const mongoose = require( 'mongoose' );
 const logger = require( 'morgan' );
 const routes = require( './routes' );
+const passport = require( 'passport' );
+const config = require( './config' );
 
 // Configure Express Sever
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use( logger( 'dev' ) );
 app.use( bodyParser.json() );
+app.use( passport.initialize() );
+
+// load passport strategies
+const localSignupStrategy = require('./passport/local-signup');
+const localLoginStrategy = require('./passport/local-login');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+// pass the authenticaion checker middleware
+const authCheckMiddleware = require('./server/middleware/auth-check');
+app.use('/api', authCheckMiddleware);
 
 // Connect mongoose to database
 mongoose.Promise = global.Promise;
