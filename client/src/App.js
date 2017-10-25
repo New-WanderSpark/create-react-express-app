bfae84d49a378fb48fa33ac03677d3d884935d18
John Desrosiers <johndrunner@yahoo.com>
2017-10-25 06:55:57 -0700

    fixed eslint error and clicking 'register now' jumpse to registration form

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,34 @@
 import React, { Component } from 'react';
-import LoginRegisterPanel from "./components/landingPage/Login-RegisterPanel/login-registerPanel/login-registerPanel";
-import Logo from "./components/landingPage/logo/logo";
-import About from "./components/landingPage/about/about";
+import LoginRegisterPanel from './components/landingPage/Login-RegisterPanel/login-registerPanel/login-registerPanel';
+import Logo from './components/landingPage/logo/logo';
+import About from './components/landingPage/about/about';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
 
+// TODO:
+// * Find out if there is a way to render all components for a path with a single route component and refactor
+// * All component names and component module names should should be capitalized
 
 class App extends Component {
-  render() {
-    return (
-      <Router>  
-        <div>
-        <Route exact={true} path="/" component={Logo}/>
-        <div className="Buttons">
-          <Route exact={true} path="/" component={LoginRegisterPanel}/> 
-       
-        </div>
-      
-         <div>
-           <Route exact={true} path="/" component={About} />
-           <Route exact={true} path="/dashboard" component={Dashboard} />
-         </div>
-         </div>
-        </Router>
+    render () {
+        return (
+            <Router>
+                <div>
+                    <Route exact={true} path="/" component={Logo}/>
+                    <div className="Buttons">
+                        <Route exact={true} path="/" component={LoginRegisterPanel}/>
+
+                    </div>
+
+                    <div>
+                        <Route exact={true} path="/" component={About} />
+                        <Route exact={true} path="/dashboard" component={Dashboard} />
+                    </div>
+                </div>
+            </Router>
         );
     }
 }
-
 
 export default App;
--- client/src/components/landingPage/Login-RegisterPanel/login-registerPanel/login-registerPanel.js
+++ client/src/components/landingPage/Login-RegisterPanel/login-registerPanel/login-registerPanel.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
-import {Button,Icon,Form,Row,Input,Col} from 'react-materialize';
+import {Button, Icon, Form, Row, Input, Col} from 'react-materialize';
 import './login-registerPanel.css';
 
 class LoginRegisterPanel extends Component {
-    render() {
-        return(
-        <div className="container-fluid">
-            <div className="login-container">
-            <Row>
-                <Col  s={12} m={6} l={12}>
-                <Input s={6} label="Username" validate><Icon>account_box</Icon></Input>
-                </Col>
-            </Row>
-                <Row>
-                <Col s={12} m={6} l={12}>
-                <Input s={6} label="Password" validate><Icon>lock</Icon></Input>
-                </Col>
-            
-            </Row>
-                <Button id="login-button" waves='light'><i class="fa fa-sign-in">Login</i></Button>
+    render () {
+        return (
+            <div className="container-fluid">
+                <div className="login-container">
+                    <Row>
+                        <Col s={12} m={6} l={12}>
+                            <Input s={6} label="Username" validate><Icon>account_box</Icon></Input>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col s={12} m={6} l={12}>
+                            <Input s={6} label="Password" validate><Icon>lock</Icon></Input>
+                        </Col>
 
-                <h6><a href="#">Register Now!</a></h6>
+                    </Row>
+                    <Button id="login-button" waves='light'><i class="fa fa-sign-in">Login</i></Button>
+
+                    <h6><a href="#registerForm">Register Now!</a></h6>
+                </div>
             </div>
-        </div>
         );
     }
-    
 }
 
 export default LoginRegisterPanel;
