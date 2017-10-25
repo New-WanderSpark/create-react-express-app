a5bc00f1480b7693b347538d2898131089183e3f
Julie Ching <juliemching@gmail.com>
2017-10-25 09:12:18 -0700

    updated the index to include semicolon

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Login from './login/componet/Login'
+import Login from './login/componet/Login';
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
