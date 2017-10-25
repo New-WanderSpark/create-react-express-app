bfa32a9e74818095a196deab108461d09216a899
John Desrosiers <johndrunner@yahoo.com>
2017-10-24 17:53:46 -0700

    Removed router from Dashboard.js and fixed linting errors

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import './Dashboard.css';
-import searchMenu from "../searchMenu";
-import userFAB from "../userFAB";
-import Banner from "../Banner";
-import Corkboard from "../Corkboard";
-import footer from "../Footer";
+import searchMenu from '../searchMenu';
+import userFAB from '../userFAB';
+import Banner from '../Banner';
+import Corkboard from '../Corkboard';
+import Footer from '../Footer';
 
-const Dashboard = () =>
-  <Router>
-    <div>
-      <Route exact={true} path="/dashboard" component={searchMenu}/> 
-      <Route exact={true} path="/dashboard" component={userFAB}/> 
-      <Route exact={true} path="/dashboard" component={Banner}/> 
-      <Route exact={true} path="/dashboard" component={Corkboard}/>
-      <Route exact={true} path="/dashboard" component={footer}/>
-    </div>
-  </Router>;
-
+function Dashboard () {
+    return (
+        <div>
+            <searchMenu />
+            <userFAB />
+            <Banner />
+            <Corkboard />
+            <Footer />
+        </div>
+    );
+}
 export default Dashboard;
