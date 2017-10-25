c904f216dc4d4f38e6e555a04d4e83e06e614f90
mattcarr09 <mattcarrillo09@gmail.com>
2017-10-25 13:42:42 -0700

    Updated Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import LoginRegisterPanel from './components/landingPage/Login-RegisterPanel/login-registerPanel/login-registerPanel';
-import Logo from './components/landingPage/logo/logo';
-import About from './components/landingPage/about/about';
+import Logo from './components/LoginPage/logo/logo';
+import LoginButton from './components/LoginPage/loginButton/loginButton';
+import RegisterButton from './components/LoginPage/registerButton/registerButton';
+import About from './components/LoginPage/About/About';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import RegisterForm from './components/LoginPage/registerForm/registerForm';
+import LoginModal from './components/LoginPage/loginModal/loginModal'
 
 // TODO:
 // * Find out if there is a way to render all components for a path with a single route component and refactor
@@ -16,15 +19,16 @@ class App extends Component {
             <Router>
                 <div>
                     <Route exact={true} path="/" component={Logo}/>
-                    <div className="Buttons">
-                        <Route exact={true} path="/" component={LoginRegisterPanel}/>
+                    <Route exact={true} path="/" component={LoginButton}/>
+                    <Route exact={true} path="/" component={RegisterButton}/>
+                    <Route exact={true} path="/" component={About}/>
+                    <Route exact={true} path="/" component={RegisterForm}/>
+                    <Route exact={true} path='/' component={LoginModal}/>
+                
 
-                    </div>
-
-                    <div>
-                        <Route exact={true} path="/" component={About} />
-                        <Route exact={true} path="/dashboard" component={Dashboard} />
-                    </div>
+                
+                      
+                   <Route exact={true} path="/dashboard" component={Dashboard} />
                 </div>
             </Router>
         );
