45ffd6712e2a774fb725aee3552f34c8c3fd3b69
John Desrosiers <johndrunner@yahoo.com>
2017-10-25 11:32:45 -0700

    google places search renders on dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,34 +1,61 @@
 import React, { Component } from 'react';
 import './Dashboard.css';
-import SearchMenu from "../SearchMenu";
-import UserFAB from "../UserFAB";
-import Banner from "../Banner";
-import Corkboard from "../Corkboard";
-import Footer from "../Footer";
+import SearchMenu from '../SearchMenu';
+import UserFAB from '../UserFAB';
+import Banner from '../Banner';
+import Corkboard from '../Corkboard';
+import Footer from '../Footer';
+import PlacesSearchContainer from '../PlacesSearch/PlacesSearchContainer';
+// import ViewPlaceDialog from '../ViewPlaceDialog';
 
 class Dashboard extends Component {
-  render() {
-    return (
-      <div>
-        <SearchMenu />
-        <UserFAB />
-        <Banner />
-        <Corkboard />
-        <Footer />
-    </div>
-    );
-  }
+    constructor ( props ) {
+        super( props );
+        this.state = {
+            'placeDialogOpen': false,
+            'selectedPlace': {}
+        };
+    }
+
+    // set the current place for the place details dialog and show the dialog
+    showPlaceDialog ( place ) {
+        this.setState( { 'selectedPlace': place, 'placeDialogOpen': true } );
+    }
+
+    // close the place details dialog
+    closePlaceDialog () {
+        this.setState( { 'placeDialogOpen': false } );
+    }
+
+    // pins place to the place collection
+    pinPlace ( place ) {
+        // TODO add procedure to add the place to the collection of places pinned to the main trip area
+        // and send request to save the trip in the data base
+        window.alert( `Add place to trip:\ntitle: ${place.title}\nplaceId: ${place.placeId}` );
+        this.closePlaceDialog();
+    }
+    render () {
+        return (
+            <div>
+                <SearchMenu>
+                    <PlacesSearchContainer
+                        handleViewPlaceDetails={place => this.showPlaceDialog( place )}
+                        handlePinPlace={place => this.pinPlace( place )}
+                    />
+                </SearchMenu>
+                <UserFAB />
+                <Banner />
+                <Corkboard />
+                <Footer />
+                {/* <ViewPlaceDialog
+                    open={this.state.placeDialogOpen}
+                    place={this.state.selectedPlace}
+                    handlePin={place => this.pinPlace( place )}
+                    handleClose={() => this.closePlaceDialog()}
+                /> */}
+            </div>
+        );
+    }
 }
 
-function Dashboard () {
-    return (
-        <div>
-            <searchMenu />
-            <userFAB />
-            <Banner />
-            <Corkboard />
-            <Footer />
-        </div>
-    );
-}
 export default Dashboard;
--- client/src/components/SearchMenu/SearchMenu.js
+++ client/src/components/SearchMenu/SearchMenu.js
@@ -1,31 +1,31 @@
-import React from "react";
-import "./SearchMenu.css";
+import React from 'react';
+import './SearchMenu.css';
 import { Button, SideNav, SideNavItem } from 'react-materialize';
 // import PlacesSearchComponent from './placesSearch/PlacesSearchContainer';
 
-const SearchMenu = () =>
-  <SideNav
-    trigger={<Button floating large className='searchbtn' waves='light' icon='add' />}
-    options={{ closeOnClick: true }}
+const SearchMenu = ( props ) =>
+    <SideNav
+        trigger={<Button floating large className='searchbtn' waves='light' icon='add' />}
+        options={{ 'closeOnClick': true }}
     >
 
-    <SideNavItem userView
-      user={{
-        background: 'amihere',
-        image: 'amihere',
-        name: 'John Doe',
-        email: 'jdandturk@gmail.com'
-      }}
-    />
-    {placeSearch}
+        <SideNavItem userView
+            user={{
+                'background': 'amihere',
+                'image': 'amihere',
+                'name': 'John Doe',
+                'email': 'jdandturk@gmail.com'
+            }}
+        />
+        {props.children}
 
-    {/* <SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
+        {/* <SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
     <SideNavItem href='#!second'>Second Link</SideNavItem>
     <SideNavItem divider />
     <SideNavItem subheader>Subheader</SideNavItem>
     <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem> */}
-    
-    {/* <PlacesSearchComponent /> */}
-  </SideNav>
+
+        {/* <PlacesSearchComponent /> */}
+    </SideNav>;
 
 export default SearchMenu;
