0841f6a5390a538991bcce80ed02de4f412ee51b
John Desrosiers <johndrunner@yahoo.com>
2017-10-24 19:02:16 -0700

    fix linting errors in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
-import Dashboard from "./components/Dashboard";
+import Dashboard from './components/Dashboard';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Route exact={true} path="/dashboard" component={Dashboard}/> 
-        </div>
-      </Router>
-    );
-  }
+    render () {
+        return (
+            <Router>
+                <div>
+                    <Route exact={true} path="/dashboard" component={Dashboard}/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
