2af21802aabae7607e4083b45d5c36ee4289127b
John Desrosiers <johndrunner@yahoo.com>
2017-10-27 07:51:32 -0700

    add api key to google maps

--- client/src/components/Maps/Maps.js
+++ client/src/components/Maps/Maps.js
@@ -5,7 +5,7 @@ import MapStyles from "./MapStyles.json";
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,placeskey=AIzaSyAg1lkAlzKUw2hoZnMjiHSMGDcg2q3_2Go",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
