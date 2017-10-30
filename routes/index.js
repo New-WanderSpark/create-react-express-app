96f2ac5073e468af7f74a3c2d5c2088c4e1d9839
Julie Ching <juliemching@gmail.com>
2017-10-29 20:57:21 -0700

    Got auth back in place. Using single main API module to make requests.
    This allows for the consistent setting of headers like authorization, so that the JWT can be easily sent with all requests.

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,17 @@ const router = require( 'express' ).Router();
 const places = require( './places' );
 const users = require( './users' );
 const trips = require( './trips' );
-// const auth = require( './auth' );
+const auth = require( './auth' );
 
 // API Routes
-// router.use( '/', auth );
+router.use( '/', auth );
 router.use( '/api/users', users );
 router.use( '/api/trips', trips );
-router.use( '/places', places );
+/**
+ * BDC - Added /api/ here so that the places API is behind an authenticated request. Without this, anyone can hit the /places routh without being authenticated first.
+ */
+router.use( '/api/places', places );
+
 // If no API routes are hit, send the React app
 router.use( ( req, res ) => {
     if ( process.env.NODE_ENV === 'production' ) {
