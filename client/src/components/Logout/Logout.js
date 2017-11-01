7d0587542f41febeeb944c7693bad7b1b7484b98
Jasmine Kim <jasminepkim@gmail.com>
2017-11-01 11:25:36 -0700

    logout component created & test logout page created

---
+++ client/src/components/Logout/Logout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import { Switch, Route } from 'react-router-dom';
+
+class Logout extends Component {
+    render () {
+        return (
+            <h1 className="loading-text">
+            Logging out...
+            </h1>
+        );
+    }
+}
+
+export default Logout;
