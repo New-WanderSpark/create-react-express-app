34ad7226e8e086afd48b8942e6e3432a1280a16a
John Desrosiers <johndrunner@yahoo.com>
2017-11-02 13:42:35 -0700

    added scrolling when register or login buttons are clicked

--- client/src/components/LoginPage/registerButton/registerButton.js
+++ client/src/components/LoginPage/registerButton/registerButton.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import { Button, Icon } from 'react-materialize';
 import { Link } from 'react-router-dom';
 import './registerButton.css';
+import PropTypes from 'prop-types';
 
 class RegisterButton extends Component {
     render () {
         return (
-            // <a href='#registerForm'>
-            <Link to ='/'>
+            <Link
+                to={{
+                    'pathname': '/',
+                    'hash': '#form'
+                }}
+                onClick={this.props.onClick}
+            >
                 <Button className='registerBtn' waves='light'>Register
                     <Icon left>cloud</Icon>
                 </Button>
             </Link>
-            // </a>
         );
     }
 }
+
+RegisterButton.propTypes = { 'onClick': PropTypes.func };
 
 export default RegisterButton;
