caa26137400dc4d77dfa00d390df2049f9c8c5b4
John Desrosiers <johndrunner@yahoo.com>
2017-11-02 07:21:29 -0700

    ViewPlaceDialog image is positioned and sized

--- client/src/components/ViewPlaceDialog/ViewPlaceDialog.js
+++ client/src/components/ViewPlaceDialog/ViewPlaceDialog.js
@@ -3,6 +3,7 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import PropTypes from 'prop-types';
 import PlaceData from '../../lib/PlaceData';
+import { Row, Col } from 'react-materialize';
 
 /**
  * A modal dialog can only be closed by selecting one of the actions.
@@ -24,16 +25,25 @@ class ViewPlaceDialog extends React.Component {
             />
         ];
         const hasValidPlace = Boolean( this.props.place );
+        const image = <img 
+            src={hasValidPlace ? this.props.place.getImgUrl() : ''}
+            className='img-responsive z-depth-3'
+            style={{'maxHeight': '250px'}}
+        />;
+        const imageStyles = {
+            'width': '100%',
+            'maxHeight': '50%'
+        };
 
         return (
             <Dialog
-                title={hasValidPlace ? this.props.place.title : ''}
                 actions={actions}
                 modal={false}
                 open={this.props.open}
             >
-                <img src={hasValidPlace ? this.props.place.getImgUrl() : ''} />
-                {hasValidPlace ? <pre>{JSON.stringify( this.props.place, null, 2 )}</pre> : 'No Place Selected'}
+                <Row><Col s={12} className='center-align white'>{image}</Col></Row>
+                
+                {/* hasValidPlace ? <pre>{JSON.stringify( this.props.place, null, 2 )}</pre> : 'No Place Selected' */}
             </Dialog>
 
         );
